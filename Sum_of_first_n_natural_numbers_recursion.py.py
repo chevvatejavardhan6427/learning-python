c873589b83f04af03d103f3fef46ee55ea775f67
chevvatejavardhan6427 <chevvatejavardhan@gmail.com>
2025-08-01 09:28:41 +0530

    Update Sum_of_first_n_natural_numbers_recursion.py

--- Sum_of_first_n_natural_numbers_recursion.py.py
+++ Sum_of_first_n_natural_numbers_recursion.py.py
@@ -1,7 +1,10 @@
 #this function is used to calculate first n natural numbers using recursion
 def sum(n):
-	return (n*(n+1))/2
 	
+	if n==1 :
+		return 1
+	return n+sum(n-1)
 	
-n=int(input("enter a number "))	
-print(f"sum of first {n} natural numbers=",sum(n))
+n=int(input("enter a number :"))
+print(f" sum of first {n} natural numbers={sum(n)}")
+	
