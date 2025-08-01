44af95de6606bb29b305d4ce022b932b43eb55fc
chevvatejavardhan6427 <chevvatejavardhan@gmail.com>
2025-08-01 08:03:41 +0530

    Update Fahrenheit_to_Celsius.py

--- Fahrenheit_to_Celsius.py
+++ Fahrenheit_to_Celsius.py
@@ -1,6 +1,6 @@
-#this function is used to convert fahrenheit scale to Celsius scale using recursion
+#this function is used to convert fahrenheit scale to Celsius scale using recursio F_C()
 def F_C():
 	f=int(input("enter the temp. in f :"))
 	return 5/9*(f-32) 
-	
-print(F_C(),"C")
+d=F_C()	
+print(f"{round(d,2)} C")
