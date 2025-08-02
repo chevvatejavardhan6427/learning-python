39278bc3b49f3753a4b337424511385349dba7a5
chevvatejavardhan6427 <chevvatejavardhan@gmail.com>
2025-08-02 18:21:02 +0530

    Update Snake_water_gun.py

--- Snake_water_gun.py
+++ Snake_water_gun.py
@@ -1,10 +1,14 @@
-''This project is used to play the game which i used to play in my childhood and named as rock,paper and  Scissors''' 
+'''This project is used to play the game which i used to play in my childhood and named as rock,paper and  Scissors''' 
 
 def snake_water_gun(you,opponent):
+					options=["snake","water","gun"]
+					if (you not in options or opponent not in options):
+						print(" invalid input choose only from the given options")
+						return
 					if(you==opponent):
 						print("draw")
 					elif(you=='snake' and opponent=='water'):
-						print("a won the game")
+						print("you won the game")
 					elif (you=='water' and opponent=='gun'):
 					        print("you won the game")
 					elif (you=='gun' and opponent=='snake'):
@@ -14,8 +18,8 @@ def snake_water_gun(you,opponent):
 	
 		  
 		  
-you=input(" you enter your choice :") 
-opponent=input("opponent enter your choice :")
+you=input(" you enter your choice :").lower()
+opponent=input("opponent enter your choice :").lower()
 snake_water_gun(you,opponent)
 		  
 
