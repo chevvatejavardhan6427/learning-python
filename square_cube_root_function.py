7f18c73d25867515386c75d16b99337daaeec889
chevvatejavardhan6427 <chevvatejavardhan@gmail.com>
2025-08-06 19:19:05 +0530

    Create square_cube_root_function.py
    # Python Function: Square, Cube, and Square Root
    
    This simple Python program defines a function called `square_cube_square_root()` that:
    - Takes a number as input
    - Prints its square, cube, and square root
    
    ### 🧠 Concepts Used:
    - Python functions
    - Basic math operations
    - User input/output
    - `math` module for square root
    
    ### ✅ Example:

---
+++ square_cube_root_function.py
@@ -0,0 +1,8 @@
+import math
+def square_cube_squareroot_(n):
+	print(f"square of {n}={n**2}")
+	print(f"cube of {n}={n**3}")
+	print(f"square root of {n}={math.sqrt(n)}")
+	
+a=int(input("enter a number:"))
+square_cube_squareroot_(a)
