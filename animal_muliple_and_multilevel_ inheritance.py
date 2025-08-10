930907ad1b23612f7c23dbf75efcb998a4f9b1ed
chevvatejavardhan6427 <chevvatejavardhan@gmail.com>
2025-08-10 18:47:52 +0530

    Create animal_muliple_and_multilevel_ inheritance.py
    This Python program demonstrates single and multiple inheritance using an animal hierarchy. It defines base classes for animals, prey, and predators, then extends them into specific animals such as Goat, Lion, and Bear. The Bear class showcases multiple inheritance by inheriting behaviors from both prey and predator classes.

---
+++ animal_muliple_and_multilevel_ inheritance.py
@@ -0,0 +1,26 @@
+class Animal:
+		def __init__(self,name):
+			self.name=name
+		def eat(self):
+			print(f"{self.name} is eating")
+		def sleep(self):
+			print(f"{self.name} is sleeping")
+class prey(Animal):
+	def flee( self):
+		print(f"{self.name} is fleeing")
+class predator(Animal):
+	def hunt(self):
+		print(f"{self.name} is hunting")
+class goat(prey):
+	pass
+class lion(predator):
+	pass
+class bear(prey,predator):
+	pass
+g=goat("deekshu")
+l=lion("nandhu")
+b=bear("vinay")
+l.eat()
+l.sleep()
+l.hunt()
+l.flee()
