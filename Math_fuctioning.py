cef3c10cabea31ba64d2946f648d62881384783c
chevvatejavardhan6427 <chevvatejavardhan@gmail.com>
2025-08-22 19:23:14 +0530

    Update Math_fuctioning.py
    Basic math calculations in python

--- Math_fuctioning.py
+++ Math_fuctioning.py
@@ -2,6 +2,16 @@
 a=2
 b=a**3
 print(b) #output is "8=2³"
+
+
 #to round off up to certain number ,
 a=19.6667890459395
 print(round(a,2)) #out put is "19.67"
+
+
+#to get reminder when two numbers are divided, 
+a=10
+b=3
+c=a%b
+print(c)
+
