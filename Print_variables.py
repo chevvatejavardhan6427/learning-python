9383156109195efa61dfd275be6a0c487cca5bc4
chevvatejavardhan6427 <chevvatejavardhan@gmail.com>
2025-08-22 15:55:12 +0530

    Create Print_variables.py
    A variable can be printed in three different ways depends on it's data type.

---
+++ Print_variables.py
@@ -0,0 +1,7 @@
+'''VARIABLES : A reusable containers for storing a value.'''
+#three ways to print variable depend on their data type
+
+age=18
+print("I am",age," years old")
+print("I am "+str(age)+" years old")
+print(f"I am {age} years old")
