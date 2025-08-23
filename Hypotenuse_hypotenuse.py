24687718e901867a378e8542cd6ecb581734a1d3
chevvatejavardhan6427 <chevvatejavardhan@gmail.com>
2025-08-23 16:41:30 +0530

    Update Hypotenuse_hypotenuse.py
    Used some basic operations to find hypotenuse of a triangle

--- Hypotenuse_hypotenuse.py
+++ Hypotenuse_hypotenuse.py
@@ -1,5 +1,5 @@
 import math 
 b=int(input("enter the base :  " ))
 h=int(input("enter the height : "))
-hypo=math.sqrt(b**2+h**2)
+hypo=math.sqrt(pow(b,2)+pow(h,2))
 print(f"length of hypotenuous = {round(hypo,2)}cm")
