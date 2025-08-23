df9daf90485f0028a6673149907efa5579b8a54e
chevvatejavardhan6427 <chevvatejavardhan@gmail.com>
2025-08-23 21:22:34 +0530

    Update and rename Kg_pounds.py to Kg_pounds_ifelse.py
    This code is used in the conversion between kg to pound and pound to kg

--- Kg_pounds_ifelse.py
+++ Kg_pounds_ifelse.py
@@ -2,5 +2,7 @@ weight=float(input("Enter your weight : "))
 unit=(input("Enter unit in which weight is measured : ").lower())
 if unit=="kg":
 	print(f"weight in pounds ={round(2.205*weight,2)}lb")
-else:
+elif unit=="pound":
 	print(f"weight in kg ={round(weight/2.205,2)}kg")
+else:
+	print("you have entered invalid unit.") 
