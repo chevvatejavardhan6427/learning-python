a0a340e6dc26264f125332d84bb5eb751183dcdb
chevvatejavardhan6427 <chevvatejavardhan@gmail.com>
2025-08-23 15:43:23 +0530

    Update Math_fuctioning.py
    Some helpful python mathematical operations

--- Math_fuctioning.py
+++ Math_fuctioning.py
@@ -7,6 +7,8 @@ print(b) #output is "8=2³"
 #to round off up to certain number ,
 a=19.6667890459395
 print(round(a,2)) #out put is "19.67"
+a=7.77
+print(round(a))#out put is 8
 
 
 #to get reminder when two numbers are divided, 
@@ -15,3 +17,11 @@ b=3
 c=a%b
 print(c)
 
+#"[pow(z,which power of z you want)]it is used to rise the power of z you want  
+print(pow(6,2))
+
+#mathematical operations 
+import math
+print(math.sqrt(9))# to get square root
+print(math.pi)#π value
+print(math.e)# e value
