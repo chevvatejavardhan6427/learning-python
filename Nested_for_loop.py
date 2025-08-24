d0d4a4909f332cea2d39f5af6b2a645e31342f45
chevvatejavardhan6427 <chevvatejavardhan@gmail.com>
2025-08-24 17:47:53 +0530

    Create Nested_for_loop.py
    We can print any symbol in rectangle or square with that symbols arranging them in rows and columns with nested for loop

---
+++ Nested_for_loop.py
@@ -0,0 +1,7 @@
+rows=int(input("enter no of rows : "))
+columns=int(input("enter no of columns :"))
+symbol=input("enter a symbol : ")
+for k in range(rows):
+	for i in range(columns):
+		print(symbol,end="")
+	print()
