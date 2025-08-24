e6d708bb93f47d957975da5f6958f5bc903793cd
chevvatejavardhan6427 <chevvatejavardhan@gmail.com>
2025-08-24 10:57:45 +0530

    Create methods_string.py
    Some important string methods

---
+++ methods_string.py
@@ -0,0 +1,16 @@
+n = "Teja"
+
+print(
+    "Length:", len(n),
+    "Spaces:", n.count(" "),
+    "Find d:", n.find("d"),
+    "Find j:", n.find("j"),
+    "Find z:", n.find("z"),
+    "Capitalize:", n.capitalize(),
+    "Upper:", n.upper(),
+    "Lower:", n.lower(),
+    "Title:", n.title(),
+    "Is Digit:", n.isdigit(),
+    "Is Alpha:", n.isalpha(),
+    "Replace e→h:", n.replace("e","h")
+)
