a49c752f8ac5a53e46a877a50b732acb3dc18763
chevvatejavardhan6427 <chevvatejavardhan@gmail.com>
2025-08-30 07:44:02 +0530

    Create Methods_lists.py
    Remove comment to check the code

---
+++ Methods_lists.py
@@ -0,0 +1,37 @@
+# list_methods_demo.py
+# Author: Teja Vardhan Chevva
+# Description: Demonstrates different Python list operations with examples.
+# Tip: Uncomment a line to test the operation.
+
+animals = ['lion', 'tiger', 'elephant', 'deer', 'bear']
+
+# 1. Accessing elements and slicing
+print("Slicing:", animals[::2])  # Every 2nd element
+print("Length:", len(animals))
+
+# 2. Adding elements
+# animals.append('panda')
+# animals.insert(3, 'buffalo')
+
+# 3. Updating elements
+# animals[1] = 'wolf'
+
+# 4. Removing elements
+# animals.remove('deer')
+
+# 5. Sorting and reversing
+# animals.sort()
+# animals.reverse()
+
+# 6. Searching
+# print("Index of tiger:", animals.index('tiger'))
+# print("Is snake in animals?", 'snake' in animals)
+
+# 7. Counting occurrences
+# print("Count of tiger:", animals.count('tiger'))
+
+# 8. Looping through list
+# for animal in animals:
+#     print(animal)
+
+print("Final List:", animals)
