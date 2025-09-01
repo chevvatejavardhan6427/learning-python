5bf1730f5a016c6f0d89437fcff2eb5803b1be29
chevvatejavardhan6427 <chevvatejavardhan@gmail.com>
2025-09-01 17:55:02 +0530

    Create Dictionary_methods.py

---
+++ Dictionary_methods.py
@@ -0,0 +1,29 @@
+d={"bird":"peacock",
+"animal":"tiger",
+"fruit":"mango"}
+#print(d.get('animal'))
+'''d.update({"bird":"banyan"})
+print(d)'''
+'''if d.get("sport"):
+	print("value exists")
+else:
+	print("value doesnot exist")'''
+'''d.clear()
+print(d)'''
+'''d.pop('animal')
+print(d)'''
+
+'''d.popitem()
+print(d)'''
+#print(d.keys())
+#print(d.values())
+'''a=d.keys()
+for key in a:
+	print(key)'''
+'''a=d.values()
+for value in a:
+	print(value)'''
+'''a=d.items()
+for key,value in a:
+	print(f"{key}:{value}")'''
+#print(d.items())
